923dbb28a2cc2d5ee55ab0d2f7af4b8c400cb2a1
pengzhimingcn@163.com <pzmcn163.com.cnes>
2018-06-10 00:06:49 +0800

    配置

---
+++ app/src/main/java/com/github/biasai/repositories/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.github.biasai.repositories
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        var displayMetrics = getResources().getDisplayMetrics()
+
+        Log.e("test","widthd:\t"+displayMetrics.widthPixels+"\theight:\t"+displayMetrics.heightPixels)
+//        Log.e("test","x100:\t"+resources.getDimension(R.dimen.biasx100)+"\ty100:\t"+resources.getDimension(R.dimen.biasy100))
+        Log.e("test","test5:\t"+resources.getDimension(R.dimen.test5))
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
